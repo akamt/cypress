7334691e56fefa7dadb5cb06ca35b5d77de30202
akamt <akamt68@gmail.com>
2021-06-24 21:30:26 +0900

    indexページのテストを追加した。

---
+++ cypress/integration/index.spec.ts
@@ -0,0 +1,10 @@
+context("/ - Home", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("ページが表示されている", () => {
+    cy.location("pathname", { timeout: 10000 }).should("include", "/");
+    cy.get("[data-cy=welcome]").contains("Welcome to Next.js!");
+  });
+});
