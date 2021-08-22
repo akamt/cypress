8b401fe88dc72d6b3e16df9ce9920acda67c96a5
akamt <akamt68@gmail.com>
2021-08-22 16:01:14 +0900

    レイアウトをしてみた。

--- src/pages/antd.tsx
+++ src/pages/antd.tsx
@@ -1,36 +1,72 @@
-import React, { useState } from "react";
-import { DatePicker, message, Alert, Button } from "antd";
-import { Moment } from "moment";
+import React from "react";
+import { Layout, Menu, Breadcrumb } from "antd";
 
-const AntdPage = (): JSX.Element => {
-  const [date, setDate] = useState<Moment>();
+import {
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
+} from "@ant-design/icons";
 
-  const handleChange = async (value: Moment) => {
-    await message.info(
-      `Selected Date: ${value ? value.format("YYYY-MM-DD") : "None"}`
-    );
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 
-    await setDate(value);
-  };
-
-  return (
-    <div style={{ width: 400, margin: "100px auto" }}>
-      <DatePicker onChange={handleChange} />
-      <div style={{ marginTop: 16 }}>
-        <Alert
-          message="Selected Date"
-          description={date ? date.format("YYYY-MM-DD") : "None"}
-        />
-        <div style={{ display: "flex", padding: "16px" }}>
-          <Button type="primary">primary button</Button>
-          <Button type="ghost">ghost button</Button>
-          <Button type="dashed">dashed button</Button>
-          <Button type="link">link button</Button>
-          <Button type="text">text button</Button>
-        </div>
-      </div>
-    </div>
-  );
-};
+const AntdPage = (): JSX.Element => (
+  <Layout style={{ width: "100vw", height: "100vh" }}>
+    <Header className="header">
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+        <Menu.Item key="1">nav 1</Menu.Item>
+        <Menu.Item key="2">nav 2</Menu.Item>
+        <Menu.Item key="3">nav 3</Menu.Item>
+      </Menu>
+    </Header>
+    <Layout>
+      <Sider width={200} className="site-layout-background">
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          defaultOpenKeys={["sub1"]}
+          style={{ height: "100%", borderRight: 0 }}
+        >
+          <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
+            <Menu.Item key="1">option1</Menu.Item>
+            <Menu.Item key="2">option2</Menu.Item>
+            <Menu.Item key="3">option3</Menu.Item>
+            <Menu.Item key="4">option4</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+            <Menu.Item key="5">option5</Menu.Item>
+            <Menu.Item key="6">option6</Menu.Item>
+            <Menu.Item key="7">option7</Menu.Item>
+            <Menu.Item key="8">option8</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
+            <Menu.Item key="9">option9</Menu.Item>
+            <Menu.Item key="10">option10</Menu.Item>
+            <Menu.Item key="11">option11</Menu.Item>
+            <Menu.Item key="12">option12</Menu.Item>
+          </SubMenu>
+        </Menu>
+      </Sider>
+      <Layout style={{ padding: "0 24px 24px" }}>
+        <Breadcrumb style={{ margin: "16px 0" }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <Content
+          className="site-layout-background"
+          style={{
+            padding: 24,
+            margin: 0,
+            minHeight: 280,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  </Layout>
+);
 
 export default AntdPage;
